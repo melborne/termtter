8a83c2ccd0750cf177567c605297227851b1004c
kyoendo <postagie@gmail.com>
2010-06-13 09:13:22 +0900

    enhance easy_post plugin to check confirm setting and message length before act

--- lib/plugins/easy_post.rb
+++ lib/plugins/easy_post.rb
@@ -1,5 +1,10 @@
 module Termtter::Client
+  config.plugins.easy_post.set_default :shortest_message, 15
   register_hook(:easy_post, :point => :command_not_found) do |text|
-    execute("update #{text}")
+    if config.confirm && text.length > config.plugins.easy_post.shortest_message
+      execute("update #{text}")
+    else
+      raise Termtter::CommandNotFound, text
+    end  
   end
 end
