194cae025f7c4eb79781263dd0ae43ab1cb71754
kyoendo <postagie@gmail.com>
2010-10-26 22:18:54 +0900

    :change name of fuzzy_find command to search_user :add search_twilog command

--- lib/plugins/tsearch.rb
+++ lib/plugins/tsearch.rb
@@ -14,7 +14,8 @@ end
 
 module TwitterSearch
   BASE_URLS = {
-    :twitter => "http://twitter.com/"
+    :twitter => "http://twitter.com/",
+    :twilog  => "http://twilog.org/"
   }
 
   class InterfaceError < RuntimeError ; end
@@ -30,7 +31,7 @@ module TwitterSearch
     end
   end
 
-  class Fuzzy
+  class User
     include Interface
     attr_reader :users
 
@@ -55,15 +56,6 @@ module TwitterSearch
       users
     end
   
-    def googling_twitter(query)
-      Google::Search::Web.new do |s|
-        query += " site:#{BASE_URLS[:twitter]}"
-        s.query = query
-      end
-    rescue => e
-      raise SearchError, "#{e.message}\n\n#{e.backtrace}"
-    end
-
     def user_profiles
       threads, result_hash = [], {}
       @users.each do |result|
@@ -78,6 +70,16 @@ module TwitterSearch
       end
       threads.each { |th| th.join }
       result_hash
+    end
+
+    private
+    def googling_twitter(query)
+      Google::Search::Web.new do |s|
+        query += " site:#{BASE_URLS[:twitter]}"
+        s.query = query
+      end
+    rescue => e
+      raise SearchError, "#{e.message}\n\n#{e.backtrace}"
     end
 
     def parse_profile(response)
@@ -102,20 +104,60 @@ module TwitterSearch
       raise ParseError, "#{e.message}\n\n#{e.backtrace}"
     end
   end
+
+  class Twilog
+    include Interface
+    def initialize(user)
+      @user = user
+    end
+  
+    def search(query)
+      query = query.split(" ") if query.respond_to?(:split)
+      query.map! { |q| URI.escape q }
+      uri = "#{BASE_URLS[:twilog]}/tweets.cgi?id=#{@user}&word=#{query.join("+")}"
+      parse_twilog( get(uri) )
+    end
+
+    private
+    def parse_twilog(response)
+      date_css = 'div.tl-tweets' 
+      tweet_css = 'div.tl-tweet'
+      tweets = []
+      case response
+      when Net::HTTPSuccess
+        parsed_body = Nokogiri::HTML(response.body)
+        parsed_body.css(date_css).each do |node|
+          date = date_format(node.attributes['id'].value)
+          node.css(tweet_css).each do |tw|
+            text, time = tw.text.split("\nposted at ")
+            tweets << [text, DateTime.parse("#{date} #{time}")]
+          end
+        end
+      end
+      tweets
+    rescue => e
+      raise ParseError, "#{e.message}\n\n#{e.backtrace}"
+    end
+
+    def date_format(s)
+      # ex.d100923 => 2010/09/23
+      s.gsub(/\d\d/, '/\0').sub(/^d\//, '20')
+    end
+  end
 end
 
 module Termtter::Client
   register_command(
-    :name => :fuzzy_find,
-    :alias => :ff,
-    :help => ['fuzzy_find,ff QUERY', 'Twitter User Fuzzy Search'],
+    :name => :search_user,
+    :aliases => [:su, :user_search],
+    :help => ['search_user,su,user_search QUERY', 'Twitter User Search from profile alike'],
     :exec => lambda do |query|
       opts = {:size => 10, :verbose => false}
       query.gsub!(/(-l)\s*(\d+)/) { opts[:size] = $2.to_i; '' }
       query.gsub!(/-v/) { opts[:verbose] = true; ''}
       public_storage[:uris].clear
 
-      pf = TwitterSearch::Fuzzy.new(query, opts[:size])
+      pf = TwitterSearch::User.new(query, opts[:size])
       unless opts[:verbose]
         pf.users.each_with_index do |(name, uri), i|
           print "<green>#{i+1}:#{name}</green> => #{uri}\n".termcolor
@@ -132,12 +174,34 @@ module Termtter::Client
       opts[:size].times { |n| register_alias("#{n+1}", "uo #{n}") }
     end
   )
+
+  # TODO: 1) view next page result, 2) highlight query word in result, 3) open uri
+  register_command(
+    :name => :search_twilog,
+    :aliases => [:st, :twilog],
+    :help => ['search_twilog,st,twilog QUERY [from:USERNAME]', 'Twitter Search through Twilog'],
+    :exec => lambda do |query|
+      query, user = query.split("from:")
+      user = config.user_name unless user
+      result = TwitterSearch::Twilog.new(user).search(query)
+      result.each_with_index do |(text, datetime), i|
+        puts " <white>#{i+1}:</white><green>#{CGI.escapeHTML(text)}</green>".termcolor +
+             " <white>(#{datetime.strftime("%Y/%m/%d %H:%M:%S")})</white>".termcolor
+      end
+    end
+  )
 end
 
-# find Twitter users with query
-# usage: ff termtter -l30 (find top 30 termtterer)
-#        ff ruby -v (find ruby lovers in vebose mode)
+# search_user:
+# fuzzy search for Twitter users
+# usage: search_user termtter -l30 (find top 30 termtterer)
+#        search_user ruby -v (find ruby lovers in vebose mode)
 # to open urls on list, use uri-open all or just hit a number
-# require google-search library and uri-open plugin
+#
+# search_twilog:
+# search Twitter log with Twilog(http://twilog.org/)
+# usage: search_twilog vim ruby (find 'YOUR' tweets about 'vim + ruby')
+#        search_twilog macbook from:stevejobs (find Steve's tweets about 'macbook')
+#
+# require google-search, nokogiri library and uri-open plugin
 # more info => hp12c http://d.hatena.ne.jp/keyesberry/20100610/p1
-
